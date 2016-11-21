52a49c8143b4c6dbfa453a14e7c8cc0073075b04
Conny <conny_0011@hotmail.com>
2016-11-21 14:35:14 +0100

    new Project with working beans

---
+++ src/models/Medium.java
@@ -0,0 +1,50 @@
+package models;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the medium database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="Medium.findAll", query="SELECT m FROM Medium m")
+public class Medium implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private int id;
+
+	private String password;
+
+	private String username;
+
+	public Medium() {
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUsername() {
+		return this.username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+}
