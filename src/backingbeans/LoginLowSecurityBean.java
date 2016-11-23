65279bdf29acceedeee81dfd18dfc3a8dfd3be0b
Conny <conny_0011@hotmail.com>
2016-11-23 23:57:03 +0100

    added logon functionality for LowSecurity version

--- src/backingbeans/LoginLowSecurityBean.java
+++ src/backingbeans/LoginLowSecurityBean.java
@@ -2,8 +2,12 @@ package backingbeans;
 
 import java.io.Serializable;
 
+import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
+
+import ejb.interfaces.LocalLowLoginEJB;
+import entities.LowSecurityEntity;
 
 @Named(value="loginLow")
 @RequestScoped
@@ -11,4 +15,48 @@ public class LoginLowSecurityBean implements Serializable {
 
 	private static final long serialVersionUID = -7409229278212770001L;
 
+	private String username;
+	private String password;
+	private LowSecurityEntity lowSecurityEntity;
+	
+	public LowSecurityEntity getLowSecurityEntity() {
+		return lowSecurityEntity;
+	}
+
+	public void setLowSecurityEntity(LowSecurityEntity lowSecurityEntity) {
+		this.lowSecurityEntity = lowSecurityEntity;
+	}
+
+	@EJB
+	private LocalLowLoginEJB lowLoginEJB;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String login() {
+		LowSecurityEntity returnedEntity = lowLoginEJB.login(username, password);
+		
+		if(returnedEntity != null) {
+			this.lowSecurityEntity = returnedEntity;
+			return "loggedOnLowSecurity";
+		}else {
+			return "";
+		}
+	}
+	
+	
+	
 }
