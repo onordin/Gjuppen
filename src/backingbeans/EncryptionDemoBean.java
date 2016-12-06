53a19aa35cce971d44811e3665864db6cc7d87c9
Conny <conny_0011@hotmail.com>
2016-12-06 14:50:31 +0100

    added RSA DEMO to loggedOnMedium

---
+++ src/backingbeans/EncryptionDemoBean.java
@@ -0,0 +1,93 @@
+package backingbeans;
+
+import java.io.Serializable;
+
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+import org.glassfish.jersey.internal.util.collection.StringIgnoreCaseKeyComparator;
+
+import displayEntities.MediumSecurityDisplayEntity;
+import ejb.interfaces.LocalMediumLoginEJB;
+import helpers.LiveEncryption;
+import messageservice.MessageService;
+
+@Named(value="encryptionBean")
+@SessionScoped
+public class EncryptionDemoBean implements Serializable {
+
+	private static final long serialVersionUID = -5143882865656015359L;
+	
+	private LiveEncryption liveEncryption;
+	private String textEntered;
+	private String textEncrypted;
+	private String textDecrypted;
+	private String privateKey;
+	private String publicKey;
+	
+	public EncryptionDemoBean() {
+		liveEncryption = new LiveEncryption();
+	}
+	
+	public String getTextEntered() {
+		return textEntered;
+	}
+	public void setTextEntered(String textEntered) {
+		this.textEntered = textEntered;
+	}
+	public String getTextEncrypted() {
+		return textEncrypted;
+	}
+	public void setTextEncrypted(String textEncrypted) {
+		this.textEncrypted = textEncrypted;
+	}
+	public String getTextDecrypted() {
+		return textDecrypted;
+	}
+	public void setTextDecrypted(String textDecrypted) {
+		this.textDecrypted = textDecrypted;
+	}
+	public String getPrivateKey() {
+		return privateKey;
+	}
+	public void setPrivateKey(String privateKey) {
+		this.privateKey = privateKey;
+	}
+	public String getPublicKey() {
+		return publicKey;
+	}
+	public void setPublicKey(String publicKey) {
+		this.publicKey = publicKey;
+	}
+	
+	public void setupForNewDemo() {
+		System.out.println("inside setup demo");
+		liveEncryption = new LiveEncryption();
+		liveEncryption.generateKey();
+		this.privateKey = liveEncryption.getPrivateKeyAsString();
+		this.publicKey = liveEncryption.getPublicKeyAsString();
+	}
+	
+	public String encryptMessage() {
+		textEncrypted = liveEncryption.encryptAndReturnString(textEntered);
+		System.out.println("encrypted: " + textEncrypted);
+		System.out.println(liveEncryption.getPrivateKeyAsString());
+		System.out.println(liveEncryption.getPublicKeyAsString());
+		return "";
+	}
+	
+	public String decryptMessage() {
+		textDecrypted = liveEncryption.decryptAndReturnString(textEncrypted);
+		return "";
+	}
+	
+	public void resetBean() {
+		privateKey = "";
+		publicKey = "";
+		textEntered = "";
+		textEncrypted = "";
+		textDecrypted = "";
+	}
+	
+}
---
+++ src/helpers/LiveEncryption.java
@@ -0,0 +1,128 @@
+package helpers;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.Arrays;
+
+import javax.crypto.Cipher;
+
+import org.primefaces.expression.impl.ThisExpressionResolver;
+
+public class LiveEncryption {
+
+	private PublicKey publicKey;
+	private PrivateKey privateKey;
+	private final String ALGORITHM = "RSA";
+
+	public void generateKey() {
+		try {
+			final KeyPairGenerator keyGen = KeyPairGenerator.getInstance(ALGORITHM);
+			keyGen.initialize(2048);
+			final KeyPair key = keyGen.generateKeyPair();
+
+			publicKey = key.getPublic();
+			privateKey = key.getPrivate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private byte[] encrypt(String text, PublicKey key) {
+		byte[] cipherText = null;
+		try {
+			// get an RSA cipher object and print the provider
+			final Cipher cipher = Cipher.getInstance(ALGORITHM);
+			// encrypt the plain text using the public key
+			cipher.init(Cipher.ENCRYPT_MODE, key);
+			cipherText = cipher.doFinal(text.getBytes());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return cipherText;
+	}
+
+	private String decrypt(byte[] text, PrivateKey key) {
+		byte[] dectyptedText = null;
+		try {
+			// get an RSA cipher object and print the provider
+			final Cipher cipher = Cipher.getInstance(ALGORITHM);
+
+			// decrypt the text using the private key
+			cipher.init(Cipher.DECRYPT_MODE, key);
+			dectyptedText = cipher.doFinal(text);
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+		return new String(dectyptedText);
+	}
+
+	public String getPublicKeyAsString() {
+		return Integer.toString(publicKey.hashCode());
+	}
+
+
+	public String getPrivateKeyAsString() {
+		return Integer.toString(privateKey.hashCode());
+	}
+
+
+	public String encryptAndReturnString(String text) {
+		String encrypted = "";
+		byte[] cipher = encrypt(text, publicKey);
+		encrypted = Arrays.toString(cipher);
+		return encrypted;
+	}
+	
+	
+	public String decryptAndReturnString(String encryptedValue) {
+		String decrypted = "";
+		
+		String[] byteValues = encryptedValue.substring(1, encryptedValue.length() - 1).split(",");
+		byte[] bytes = new byte[byteValues.length];
+
+		for (int i=0, len=bytes.length; i<len; i++) {
+		   bytes[i] = Byte.parseByte(byteValues[i].trim());     
+		}		
+		
+		decrypted = decrypt(bytes, privateKey);
+		return decrypted;
+	}
+	
+	
+	
+	
+	/**
+	
+	public void main(String[] args) {
+
+		try {
+
+			generateKey();
+
+			final String originalText = "Text to be encrypted bla bla bla bla bla bla bla bla bla bla bla bla bla";
+
+			final byte[] cipherText = encrypt(originalText, this.publicKey);
+
+			final String plainText = decrypt(cipherText, privateKey);
+
+			System.out.println("Original: " + originalText);
+			System.out.println("Public key: " + Integer.toString(publicKey.hashCode()));
+			System.out.println("Private Key: " + Integer.toString(privateKey.hashCode()));
+
+			String encryptedString = Arrays.toString(cipherText);
+			System.out.println("Arrays to String: " + encryptedString);
+
+			System.out.println("Decrypted: " + plainText);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	*/
+	
+	
+}
