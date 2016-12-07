4025cc08445ba523cca3f19602045e3fef4306ef
Conny <conny_0011@hotmail.com>
2016-12-07 15:22:59 +0100

    updated HTTPS/RSA demo with design and AJAX calls, changed to password field on low security login

--- src/backingbeans/EncryptionDemoBean.java
+++ src/backingbeans/EncryptionDemoBean.java
@@ -1,12 +1,12 @@
 package backingbeans;
 
 import java.io.Serializable;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 
-import displayEntities.MediumSecurityDisplayEntity;
-import ejb.interfaces.LocalMediumLoginEJB;
 import helpers.LiveEncryption;
 
 @Named(value="encryptionBean")
@@ -19,13 +19,16 @@ public class EncryptionDemoBean implements Serializable {
 	private String textEntered;
 	private String textEncrypted;
 	private String textDecrypted;
-	private String privateKey;
-	private String publicKey;
+	private String privateKeyExp;
+	private String privateKeyMod;
+	private String publicKeyExp;
+	private String publicKeyMod;
 	
 	public EncryptionDemoBean() {
 		liveEncryption = new LiveEncryption();
 	}
 	
+
 	public String getTextEntered() {
 		return textEntered;
 	}
@@ -44,43 +47,66 @@ public class EncryptionDemoBean implements Serializable {
 	public void setTextDecrypted(String textDecrypted) {
 		this.textDecrypted = textDecrypted;
 	}
-	public String getPrivateKey() {
-		return privateKey;
+	public String getPrivateKeyExp() {
+		return privateKeyExp;
 	}
-	public void setPrivateKey(String privateKey) {
-		this.privateKey = privateKey;
+
+	public void setPrivateKeyExp(String privateKeyExp) {
+		this.privateKeyExp = privateKeyExp;
 	}
-	public String getPublicKey() {
-		return publicKey;
+
+	public String getPrivateKeyMod() {
+		return privateKeyMod;
 	}
-	public void setPublicKey(String publicKey) {
-		this.publicKey = publicKey;
+
+	public void setPrivateKeyMod(String privateKeyMod) {
+		this.privateKeyMod = privateKeyMod;
 	}
+
+	public String getPublicKeyExp() {
+		return publicKeyExp;
+	}
+
+	public void setPublicKeyExp(String publicKeyExp) {
+		this.publicKeyExp = publicKeyExp;
+	}
+
+	public String getPublicKeyMod() {
+		return publicKeyMod;
+	}
+
+	public void setPublicKeyMod(String publicKeyMod) {
+		this.publicKeyMod = publicKeyMod;
+	}
+
 	
 	public void setupForNewDemo() {
-		System.out.println("inside setup demo");
-		liveEncryption = new LiveEncryption();
 		liveEncryption.generateKey();
-		this.privateKey = liveEncryption.getPrivateKeyAsString();
-		this.publicKey = liveEncryption.getPublicKeyAsString();
+		RSAPublicKey publicRSAKey = (RSAPublicKey) liveEncryption.getPublicKey();
+		RSAPrivateKey privateRSAKey = (RSAPrivateKey) liveEncryption.getPrivateKey();
+		publicKeyExp = publicRSAKey.getPublicExponent().toString();
+		publicKeyMod = publicRSAKey.getModulus().toString();
+		privateKeyExp = privateRSAKey.getPrivateExponent().toString();
+		privateKeyMod = privateRSAKey.getModulus().toString();
 	}
 	
-	public String encryptMessage() {
+	public void encryptMessage() {
 		textEncrypted = liveEncryption.encryptAndReturnString(textEntered);
+		System.out.println("entered: " + textEntered);
 		System.out.println("encrypted: " + textEncrypted);
 		System.out.println(liveEncryption.getPrivateKeyAsString());
 		System.out.println(liveEncryption.getPublicKeyAsString());
-		return "";
 	}
 	
-	public String decryptMessage() {
+	public void decryptMessage() {
 		textDecrypted = liveEncryption.decryptAndReturnString(textEncrypted);
-		return "";
 	}
 	
 	public void resetBean() {
-		privateKey = "";
-		publicKey = "";
+		publicKeyExp = "";
+		publicKeyMod = "";
+		privateKeyExp = "";
+		privateKeyMod = "";
 		textEntered = "";
 		textEncrypted = "";
 		textDecrypted = "";
--- src/helpers/LiveEncryption.java
+++ src/helpers/LiveEncryption.java
@@ -4,7 +4,9 @@ import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.PrivateKey;
 import java.security.PublicKey;
-import java.util.Arrays;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.Base64;
 
 import javax.crypto.Cipher;
 
@@ -18,7 +20,7 @@ public class LiveEncryption {
 	public void generateKey() {
 		try {
 			final KeyPairGenerator keyGen = KeyPairGenerator.getInstance(ALGORITHM);
-			keyGen.initialize(2048);
+			keyGen.initialize(1024);
 			final KeyPair key = keyGen.generateKeyPair();
 
 			publicKey = key.getPublic();
@@ -44,6 +46,7 @@ public class LiveEncryption {
 
 	private String decrypt(byte[] text, PrivateKey key) {
 		byte[] dectyptedText = null;
+		String decrypted = "";
 		try {
 			// get an RSA cipher object and print the provider
 			final Cipher cipher = Cipher.getInstance(ALGORITHM);
@@ -51,28 +54,43 @@ public class LiveEncryption {
 			// decrypt the text using the private key
 			cipher.init(Cipher.DECRYPT_MODE, key);
 			dectyptedText = cipher.doFinal(text);
+			decrypted = new String(dectyptedText);
 
 		} catch (Exception ex) {
+			System.out.println("failure to decrypt");
+			decrypted = "ERROR - Failure during decryption!";
 			ex.printStackTrace();
 		}
-
-		return new String(dectyptedText);
+		return decrypted;
 	}
 
+	
 	public String getPublicKeyAsString() {
-		return Integer.toString(publicKey.hashCode());
+		RSAPublicKey pub = (RSAPublicKey) publicKey;
+		String fullKey = "";
+		fullKey += pub.getPublicExponent();
+		fullKey += ":";
+		fullKey += pub.getModulus();
+		return fullKey;
+	
 	}
-
 
 	public String getPrivateKeyAsString() {
-		return Integer.toString(privateKey.hashCode());
+		RSAPrivateKey priv = (RSAPrivateKey) privateKey;
+		String fullKey = "";
+		fullKey += priv.getPrivateExponent();
+		fullKey += ":";
+		fullKey += priv.getModulus();
+		return fullKey;
 	}
 
 
 	public String encryptAndReturnString(String text) {
 		String encrypted = "";
 		byte[] cipher = encrypt(text, publicKey);
-		encrypted = Arrays.toString(cipher);
+		
+		encrypted = Base64.getEncoder().encodeToString(cipher);
+		//encrypted = Arrays.toString(cipher);
 		return encrypted;
 	}
 	
@@ -80,15 +98,35 @@ public class LiveEncryption {
 	public String decryptAndReturnString(String encryptedValue) {
 		String decrypted = "";
 		
+		byte[] bytes = Base64.getDecoder().decode(encryptedValue);
+
+		/*
 		String[] byteValues = encryptedValue.substring(1, encryptedValue.length() - 1).split(",");
 		byte[] bytes = new byte[byteValues.length];
 
 		for (int i=0, len=bytes.length; i<len; i++) {
 		   bytes[i] = Byte.parseByte(byteValues[i].trim());     
 		}		
+		*/
 		
 		decrypted = decrypt(bytes, privateKey);
 		return decrypted;
+	}
+
+	public PublicKey getPublicKey() {
+		return publicKey;
+	}
+
+	public void setPublicKey(PublicKey publicKey) {
+		this.publicKey = publicKey;
+	}
+
+	public PrivateKey getPrivateKey() {
+		return privateKey;
+	}
+
+	public void setPrivateKey(PrivateKey privateKey) {
+		this.privateKey = privateKey;
 	}
 	
 	
